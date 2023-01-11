4f376d25c5c6bac207541b52a4e84ec86b509639
Alek666666 <alek6.don@gmail.com>
2023-01-11 11:59:17 +0100

    :fire

--- test/src/App.js
+++ test/src/App.js
@@ -1,9 +1,20 @@
+import { ColorModeContext, useMode } from './theme';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import Topbar from"./scenes/global/Topbar";
 
 function App() {
-  return (
+  const [theme, colorMode] = useMode();
+
+  return (<ColorModeContext.Provider value={colorMode}>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
     <div className="app">
-      Chips
+      <main className="content">
+        <Topbar />
+      </main>
     </div>
+    </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 }
 
